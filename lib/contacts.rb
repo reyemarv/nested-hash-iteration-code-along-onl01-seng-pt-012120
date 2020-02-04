ddb53b4a71abf17c572e1978a1e55a2cb5a93342
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 02:14:45 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,8 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, data|
-    puts "#{person}: #{data}"
-  end
-  data.each do |attribute, value|
+  contacts.each do |person, contact_details_hash|
+  end 
+
 end
