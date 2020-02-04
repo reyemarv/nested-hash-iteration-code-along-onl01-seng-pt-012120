3979e794e0d7a5dc2941d8e749fae0754e2eba35
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 02:17:44 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,13 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+    if person == "freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if{|ice_cream| ice_cream == "strawberry"}
+        end 
+        end 
+      end
   end 
 
 end
